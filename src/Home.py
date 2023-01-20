83d7ef56c3e902cf7d2ea4d7f1681699621638d1
Amilton Lobo <amilton.mendes@gmail.com>
2023-01-20 17:50:53 -0300

    Preparação de página inicial

--- src/Home.py
+++ src/Home.py
@@ -12,3 +12,7 @@ st.set_page_config(
 ################################## Header ######################################################
 st.markdown("""# Apresentação""")
 st.write("---")
+st.markdown("""No menu à esquerda serão encontrados dois painéis:
+ * **Comparativo de Estados** : com informações da distribuição de orçamento e número de assessores de forma a permitir a comparação de diferentes métricas entre estados;
+ * **Detalhamento de perfil por estado** : com informações da distribuição de orçamento e número de assessores para a análise de diferentes métricas em um estado selecionado.""")
+
